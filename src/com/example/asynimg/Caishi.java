9b7de242e33f51cec601cb55fbdf31e81d31a6b5
zhen <zhen@sonofsky.com>
2015-04-14 22:15:33 +0800

    finish most client shopping car's job

--- src/com/example/asynimg/Caishi.java
+++ src/com/example/asynimg/Caishi.java
@@ -3,37 +3,50 @@ package com.example.asynimg;
 import java.lang.ref.SoftReference;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
+import org.json.JSONArray;
+
+import com.example.asynimg.CaishiAdater.Callback;
 import com.handmark.pulltorefresh.library.PullToRefreshBase;
 import com.handmark.pulltorefresh.library.PullToRefreshListView;
 import com.handmark.pulltorefresh.library.PullToRefreshBase.Mode;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 
-public class Caishi extends Activity {
+public class Caishi extends Activity implements Callback{
 //======20150409==================
 	 private PullToRefreshListView mPullToRefreshListView;
 	 Map<String, SoftReference<Bitmap>>  caches;
 	 private String rid;
 //==============================================
+//=====20150414=====================
+	 private SharedPreferences shopcart;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		ExitUtil.activityList.add(this);
 		setContentView(R.layout.activity_caishi);
+		 shopcart=getSharedPreferences("mycart",0);
 		//===========20150409===================
 		final String url="http://10.0.2.2:8080/DingCan/index.php/server/showResturant/getCaibyRes";
 		caches=new HashMap<String, SoftReference<Bitmap>>();
 		mPullToRefreshListView=(PullToRefreshListView)findViewById(R.id.refresh_caishi);
 		 ListView listView1 = mPullToRefreshListView.getRefreshableView();
 	      mPullToRefreshListView.setMode(Mode.BOTH);
-	      final CaishiAdater caishiAdater=new CaishiAdater(this, new ArrayList<Map<String,String>>(), caches,mPullToRefreshListView);
+	      final CaishiAdater caishiAdater=new CaishiAdater(this, new ArrayList<Map<String,String>>(), caches,mPullToRefreshListView,this);
 	      listView1.setAdapter(caishiAdater);
 	       String  current=String.valueOf(caishiAdater.getCount());
 	       rid=getIntent().getExtras().getString("rid");
@@ -52,7 +65,37 @@ public class Caishi extends Activity {
 	        		 new CaiWorker(caishiAdater).execute(url,rid,current,num);
 	        	  }
 	        	});
-	    }
-		//==========================================
+	      //==========================================
+	       Button buycart=(Button)findViewById(R.id.buycar);
+	       TextView carinfo=(TextView)findViewById(R.id.carinfo);
+	       Button btnok=(Button)findViewById(R.id.btnok);
+	       final JSONArray dingdan=new JSONArray();
+	        btnok.setOnClickListener(new OnClickListener() {
+				
+				@Override
+				public void onClick(View arg0) {
+					// TODO Auto-generated method stub
+					Map mylist=shopcart.getAll();
+//					 Log.i("caishi size",String.valueOf(mylist.size()));
+					Iterator iterator=mylist.entrySet().iterator();
+					while(iterator.hasNext()){
+						Map.Entry entry = (Map.Entry) iterator.next();  
+					    Object key = entry.getKey();  
+					    Object val = entry.getValue(); 
+//					    Log.i("caishi buy", val.toString());
+						dingdan.put(val);
+					}
+					 Log.i("caishi buy", dingdan.toString());
+					shopcart.edit().clear().commit();
+				}
+			});
+	       
 	}
+	@Override
+	public void click(View v) {
+		// TODO Auto-generated method stub
+			TextView carinfo=(TextView)findViewById(R.id.carinfo);
+			carinfo.setText(String.valueOf(v.getTag()));
+	}
+}
 	
--- src/com/example/asynimg/CaishiAdater.java
+++ src/com/example/asynimg/CaishiAdater.java
@@ -1,19 +1,34 @@
 package com.example.asynimg;
 
 import java.lang.ref.SoftReference;
+import java.security.PublicKey;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+
+import javax.security.auth.PrivateCredentialPermission;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import com.example.asynimg.BitmapWorkerTask.AsyncDrawable;
 import com.example.asynimg.UpdateAdater2.viewholder;
 import com.handmark.pulltorefresh.library.PullToRefreshListView;
 
+import android.R.integer;
+import android.app.Activity;
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.Button;
@@ -26,12 +41,14 @@ public class CaishiAdater extends BaseAdapter{
 	private Map<String, SoftReference<Bitmap>> caches;
 	private LayoutInflater mInflater;
 	private PullToRefreshListView mListView;
-	public  CaishiAdater(Context context,List<Map<String, String>> caishi,Map<String, SoftReference<Bitmap>> caches,PullToRefreshListView mListView) {
+	private Callback mCallback;
+	public  CaishiAdater(Context context,List<Map<String, String>> caishi,Map<String, SoftReference<Bitmap>> caches,PullToRefreshListView mListView,Callback callback) {
 		this.context=context;
 		this.caishiList=caishi;
 		this.caches=caches;
 		mInflater=(LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 		this.mListView=mListView;
+		this.mCallback=callback;
 	}
 	@Override
 	public int getCount() {
@@ -54,7 +71,8 @@ public class CaishiAdater extends BaseAdapter{
 	@Override
 	public View getView(int position, View  convertView, ViewGroup parent) {
 		// TODO Auto-generated method stub
-		viewholder holder;
+		final int pos=position;
+		final viewholder holder;
 		if (convertView==null) {
 			convertView=mInflater.inflate(R.layout.caishi, null);
 			holder=new viewholder();
@@ -63,6 +81,8 @@ public class CaishiAdater extends BaseAdapter{
 			holder.price=(TextView)convertView.findViewById(R.id.cprice);
 			holder.description=(TextView)convertView.findViewById(R.id.description);
 			holder.button=(Button)convertView.findViewById(R.id.cbuy);
+			holder.delButton=(Button)convertView.findViewById(R.id.cdel);
+			holder.count=(TextView)convertView.findViewById(R.id.count);
 			convertView.setTag(holder);
 		}else {
 			holder=(viewholder)convertView.getTag();
@@ -70,6 +90,107 @@ public class CaishiAdater extends BaseAdapter{
 		holder.name.setText(caishiList.get(position).get("name"));
 		holder.price.setText(caishiList.get(position).get("price"));
 		holder.description.setText(caishiList.get(position).get("descrition"));
+		holder.button.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				Log.i("caishiadater",holder.count.getText().toString());
+				String tmp=holder.count.getText().toString();
+				int count=Integer.valueOf(tmp).intValue();
+				count++;
+				Log.i("caishiadater count",String.valueOf(count));
+				holder.count.setText(String.valueOf(count));
+				holder.count.setVisibility(View.VISIBLE);
+				holder.delButton.setVisibility(View.VISIBLE);
+				SharedPreferences sp=context.getSharedPreferences("mycart", 0);
+				Editor editor=sp.edit();
+				editor.remove(caishiList.get(pos).get("name"));
+				JSONObject item=new JSONObject();
+				try {
+					item.put("vid", caishiList.get(pos).get("vid"));
+					item.put("price", caishiList.get(pos).get("price"));
+					item.put("count", count);
+					editor.putString(caishiList.get(pos).get("name"), item.toString());
+					editor.commit();
+					
+				} catch (JSONException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				Log.i("caishiadater add", sp.getString(caishiList.get(pos).get("name"), "null"));
+				Map mylist=sp.getAll();
+				Iterator iterator=mylist.entrySet().iterator();
+			    int sum=0;
+				while(iterator.hasNext()){
+					Map.Entry entry = (Map.Entry) iterator.next();  
+				    Object key = entry.getKey();  
+				    Object val = entry.getValue(); 
+				    try {
+						JSONObject item2=new JSONObject(val.toString());
+						Log.i("caishiadater buttonclick  price", String.valueOf(item2.getInt("price")));
+						Log.i("caishiadater buttonclick  count", String.valueOf(item2.getInt("count")));
+						sum=sum+item2.getInt("price")*item2.getInt("count");
+					} catch (JSONException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				    
+				}
+				Log.i("caishiadater buttonclick  sum", String.valueOf(sum));
+				arg0.setTag(sum);
+				mCallback.click(arg0);
+			}
+		});
+		holder.delButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				String tmp=holder.count.getText().toString();
+				int count=Integer.valueOf(tmp).intValue();
+				count--;
+				holder.count.setText(String.valueOf(count));
+				if (count==0) {
+					holder.count.setVisibility(View.GONE);
+					holder.delButton.setVisibility(View.GONE);
+				}
+				SharedPreferences sp=context.getSharedPreferences("mycart", 0);
+				Editor editor=sp.edit();
+				editor.remove(caishiList.get(pos).get("name"));
+					JSONObject item=new JSONObject();
+					try {
+						item.put("vid", caishiList.get(pos).get("vid"));
+						item.put("price", caishiList.get(pos).get("price"));
+						item.put("count", count);
+						editor.putString(caishiList.get(pos).get("name"), item.toString());
+						editor.commit();
+						
+					} catch (JSONException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				Map mylist=sp.getAll();
+				Iterator iterator=mylist.entrySet().iterator();
+			    int sum=0;
+				while(iterator.hasNext()){
+					Map.Entry entry = (Map.Entry) iterator.next();  
+				    Object val = entry.getValue(); 
+				    try {
+						JSONObject item2=new JSONObject(val.toString());
+						Log.i("caishiadater buttonclick  price", String.valueOf(item2.getInt("price")));
+						Log.i("caishiadater buttonclick  count", String.valueOf(item2.getInt("count")));
+						sum=sum+item2.getInt("price")*item2.getInt("count");
+					} catch (JSONException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				    
+				}
+				arg0.setTag(sum);
+				mCallback.click(arg0);
+			}
+		});
 		Log.i("caishiadater-getview", caishiList.get(position).get("imageurl"));
 		loadBitmap(caishiList.get(position).get("imageurl"), holder.imageView);
 		return convertView;
@@ -106,10 +227,16 @@ public class CaishiAdater extends BaseAdapter{
 		TextView price;
 		TextView description;
 		Button button;
+		Button delButton;
+		TextView count;
 	}
 	public void addItems(List<Map<String, String>> rList) {
 		caishiList.addAll(rList);
 		notifyDataSetChanged();
 		mListView.onRefreshComplete();
+	}
+	//回调接口，用于activity响应listview item button点击事件
+	public interface Callback {
+		        public void click(View v);
 	}
 }
